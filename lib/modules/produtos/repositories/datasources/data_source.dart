b65b3e11f59fabb468be033da5bddd7d07907f44
ProjetoLC <105231827+ProjetoLC@users.noreply.github.com>
2022-10-05 17:32:12 -0300

    Update data_source.dart

--- lib/modules/produtos/repositories/datasources/data_source.dart
+++ lib/modules/produtos/repositories/datasources/data_source.dart
@@ -1,11 +1,11 @@
 abstract class DataSourceBase {
-  Future<void> incluir(Map<String, dynamic> usuario);
+  Future<void> incluir(Map<String, dynamic> produto);
   
-  Future<void> excluir(Map<String, dynamic> usuario);
+  Future<void> excluir(Map<String, dynamic> produto);
   
-  Future<void> alterar(Map<String, dynamic> usuario);
+  Future<void> alterar(Map<String, dynamic> produto);
   
-  Future<Map<String, dynamic>> selecionar(String email);
+  Future<Map<String, dynamic>> selecionar(String id);
 
   Future<List<Map<String, dynamic>>> selecionarTodos();
 }
